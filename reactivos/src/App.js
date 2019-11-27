c9d9069e994eec344f9bec786cb918887aae4aff
IgancioTsaii <igu99.tsai@gmail.com>
2019-11-27 18:49:36 -0300

    componente video

--- reactivos/src/App.js
+++ reactivos/src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './App.css'
 
 import Logo from './components/Logo'
-import Link from './components/Link'
+import Menu from './components/Menu'
+import Mapa from './components/Mapa'
+import Video from './components/Video'
 
 const links = [
   {
@@ -14,22 +16,32 @@ const links = [
     url : "https://reactjs.org/tutorial/tutorial.html",
     blank : false,
     text : "Tutorial"
+  },
+  {
+    url : "https://angular.io/docs",
+    blank : true,
+    text : "The Dark Side of the JS"
   }
 ]
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo category="sports" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <Link url="https://reactjs.org" blank="yes" text="Documentation" />
-          <Link url="https://reactjs.org/tutorial/tutorial.html" blank="no" text="Tutorial" />
-          { /* DESAFIO:*/ }
-          { /*<Menu items={links} />*/ }
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <Logo category="sports" />
+            <p>Edit <code>src/App.js</code> and save to reload.</p>
+            { /* DESAFIO I: */ }
+            <Menu items={links} />
+
+            { /* DESAFIO II: */ }
+            <Mapa lat="-34.6078602" long="-58.383111" zoom="15" />
+
+            {/* DESAFIO III: */}
+            <Video id="TobNCFMK_bs" play="yes" />
+
+          </header>
+        </div>
     )
   }
 }
